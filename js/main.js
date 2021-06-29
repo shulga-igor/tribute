f74eeaf7576f91ec7995381559f84363aa0c18ba
Igor Shu <shu.igor.1990@gmail.com>
2021-06-29 20:40:44 +0300

    fixed banner

--- js/main.js
+++ js/main.js
@@ -13,10 +13,12 @@ $( window ).load(function() {
         slidesToShow: 1,
         slidesToScroll: 1,
         dots: true,
-        infinite: false,
-        arrows: false,
+        infinite: true,
+        arrows: true,
         autoplay: true,
         autoplaySpeed: 7000,
+        prevArrow: '<div class="arrow-prev"><svg width="20" height="37" xmlns="http://www.w3.org/2000/svg"><path d="M4.001 18.232L19.404 2.83A1.657 1.657 0 1017.06.486L.486 17.06a1.657 1.657 0 000 2.344L17.06 35.979c.324.323.748.485 1.172.485a1.657 1.657 0 001.172-2.83L4.001 18.233z" fill="#FFF" fill-rule="nonzero"/></svg><div>',
+        nextArrow: '<div class="arrow-next"><svg width="20" height="37" xmlns="http://www.w3.org/2000/svg"><path d="M15.999 18.232L.596 2.83A1.657 1.657 0 112.94.486L19.514 17.06a1.657 1.657 0 010 2.344L2.94 35.979a1.652 1.652 0 01-1.172.485 1.657 1.657 0 01-1.172-2.83l15.403-15.402z" fill="#FFF" fill-rule="nonzero"/></svg><div>',
     });
 
 
